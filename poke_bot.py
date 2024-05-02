07d053e642364177344eb74dc0a3b670b564f071
uzi0702 <inkring1100@gmail.com>
2024-05-02 21:55:05 +0900

    send warming if bot catch pokemon that has some forms

--- poke_bot.py
+++ poke_bot.py
@@ -8,16 +8,12 @@ __author__ = "uzi"
 __version__ = "0.0"
 __date__ = "2024/4/12 (created by 2024/4/12)"
 
-from email import message
-import math
-import re
-from unittest import result
+from math import floor
 import discord
 import jaconv
 #import pokebase
 import requests
 import split_text
-import time
 import bot_token as token
 
 BASE_URL = "https://pokeapi.co/api/v2/"
@@ -32,6 +28,9 @@ class PokeClient(discord.Client):
 		"""
 		super().__init__(*args, **options)
 		self.poke_dict = get_dict_id_of_pokemon()
+		self.except_list =["ランドロス","ボルトロス", "トルネロス", "ヒヒダルマ", "シェイミ", "ジガルデ",
+							"メテノ", "コオリッポ", "ヨワシ", "ザシアン", "ザマゼンタ", "フーパ", "ネクロズマ",
+							"バドレックス", "ロトム", "デオキシス", "ロトム"]
 
 	async def on_ready(self):
 		"""
@@ -44,18 +43,30 @@ class PokeClient(discord.Client):
 	async def on_message(self, message):
 		"""
 		メッセージを受け取り、返答するメソッド
+
+		Prameters:
+		message(discord.Message):受け取ったメッセージオブジェクト
 		"""
 		if message.author == self.user:
 			return
 		if message.content.startswith("!p"):
 			await message.channel.send("起動完了！\nポケモンの名前と速度(最速、準速、無振り、下降、最遅)を入力してください！")
+
+			def check(m):
+				return m.channel == message.channel
+
 			while True:
-				receive_message = await self.wait_for("message")
+				receive_message = await self.wait_for("message", check=check)
 				message_content = receive_message.content
 				target_list = split_text.split_text(message_content)
-				ja_pokemon_name = jaconv.hira2kata(target_list[0])
-				pokemon_condition = target_list[1]
-				print(ja_pokemon_name+":"+target_list[1])
+				try:
+					ja_pokemon_name = jaconv.hira2kata(target_list[0])
+					await self.check_except_pokemon(ja_pokemon_name, message)
+					pokemon_condition = target_list[1]
+					print(ja_pokemon_name+":"+target_list[1])
+				except IndexError:
+					await message.channel.send("正しい文字を入力してください。")
+					continue
 				try:
 					pokemon_id = self.poke_dict[ja_pokemon_name]
 					print("pokemon_id:"+pokemon_id)
@@ -66,21 +77,30 @@ class PokeClient(discord.Client):
 					await self.say_real_num_of_each_rank(result, message)
 				except KeyError:
 					await message.channel.send("入力失敗、正しい文字列を入力してください")
+		return
 
 	async def say_real_num_of_each_rank(self, result, message):
+		"""
+		各ランクの実数値を計算して送信するメソッド
+		"""
 		result_string = ""
 		dict_of_rank = {-6:1/4, -5:2/7, -4:1/3, -3:2/5, -2:1/2, -1:2/3, 0:1, 1:3/2, 2:2, 3:5/2, 4:3, 5:7/2, 6:4}
 		for key, value in dict_of_rank.items():
 			if key == 0:
 				continue
-			num = math.floor(result*value)
-			result_string = result_string + "{:+d}".format(key) + f" : {num}\n"
-		print(result_string)
-		await message.channel.send(result_string)
+			num = floor(result*value)
+			result_string = f"{result_string}" + "{:+d}".format(key) + f" : {num}\n" # noqa
+		print(f"{result_string}")
+		await message.channel.send(f"{result_string}")
+		return
 
-def test():
-	poke_dict = get_dict_id_of_pokemon()
-	print(poke_dict)
+	async def check_except_pokemon(self, poke_name, message):
+		"""
+		素早さが複数あるポケモンには通常フォルムしか対応していない旨を警告するメソッド
+		"""
+		if poke_name in self.except_list:
+			await message.channel.send("このポケモンは通常フォルムの素早さで応答します。")
+		return
 
 def calc_speed(speed, condition):
 	"""
@@ -100,20 +120,7 @@ def calc_speed(speed, condition):
 		result = ((speed*2)*0.5+5)*0.9
 	else:
 		return 0
-	return math.floor(result)
-
-def main():
-	"""
-	Discord上で「!p」のコマンドを入力すると入力待ち状態となり、入力を受け取るとそのポケモンの各ランクの
-	素早さ実数値を返すbotのプログラム
-	"""
-	my_intents = discord.Intents.default()
-	my_intents.message_content = True
-
-	#scrape()
-
-	client = PokeClient(intents=my_intents)
-	client.run(token.token)
+	return floor(result)
 
 def get_num_of_speed(name):
 	"""
@@ -144,16 +151,18 @@ def get_dict_id_of_pokemon():
 		result_dict[ja_name] = en_name
 	return result_dict
 
-# def get_pokemon_japanese_name(english_name):
-# 	response = requests.get(BASE_URL + f'pokemon-species/{english_name.lower()}')
-# 	if response.ok:
-# 		data = response.json()
-# 		for name_info in data['names']:
-# 			if name_info['language']['name'] == 'ja-Hrkt':
-# 				return name_info['name']
-# 		return "日本語名が見つかりません。"
-# 	else:
-# 		return "ポケモンの情報を取得できませんでした。"
+def main():
+	"""
+	Discord上で「!p」のコマンドを入力すると入力待ち状態となり、入力を受け取るとそのポケモンの各ランクの
+	素早さ実数値を返すbotのプログラム
+	"""
+	my_intents = discord.Intents.default()
+	my_intents.message_content = True
+
+	#scrape()
+
+	client = PokeClient(intents=my_intents)
+	client.run(token.token)
 
 if __name__ == "__main__":
 	main()
